1bde9eaea044f8382f9c2d6c649dca553367a76f
fatimashafana <fatimashafana888@gmail.com>
2022-11-02 11:20:49 +0530

    fatimas updates

--- AMS-Frontend/src/app/asset-definition/asset-definition.module.ts
+++ AMS-Frontend/src/app/asset-definition/asset-definition.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AssetDefintitonListComponent } from './asset-defintiton-list/asset-defintiton-list.component';
+import { AssetDefintitonEntryComponent } from './asset-defintiton-entry/asset-defintiton-entry.component';
+import { AssetTypeEntryComponent } from './asset-type-entry/asset-type-entry.component';
+import { AssetTypeListComponent } from './asset-type-list/asset-type-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AssetDefintitonListComponent, AssetDefintitonEntryComponent, AssetTypeEntryComponent, AssetTypeListComponent],
   imports: [
     CommonModule
   ]
---
+++ AMS-Frontend/src/app/asset-definition/asset-type-entry/asset-type-entry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-asset-type-entry',
+  templateUrl: './asset-type-entry.component.html',
+  styleUrls: ['./asset-type-entry.component.scss']
+})
+export class AssetTypeEntryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
